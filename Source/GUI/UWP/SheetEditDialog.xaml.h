a1388972d72e3ffa691849984b1e0f109b644ce2
Maxime Gervais <gervais.maxime@gmail.com>
2019-02-11 12:14:33 +0100

    UWP GUI: Easy and sheet views
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

---
+++ Source/GUI/UWP/SheetEditDialog.xaml.h
@@ -0,0 +1,24 @@
+﻿//
+// SheetEditDialog.xaml.h
+// Déclaration de la classe SheetEditDialog
+//
+
+#pragma once
+
+#include "SheetEditDialog.g.h"
+
+namespace MediaInfo
+{
+    [Windows::Foundation::Metadata::WebHostHidden]
+    public ref class SheetEditDialog sealed
+    {
+    public:
+        SheetEditDialog(Platform::String^ Title, Sheet^ Content);
+    private:
+        void Add_Field(Platform::String^ Name, size_t Width, size_t StreamKind, size_t StreamPos, Platform::String^ Parameter);
+        void ContentDialog_PrimaryButtonClick(Windows::UI::Xaml::Controls::ContentDialog^ Sender, Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs^ Event);
+        void ContentDialog_SecondaryButtonClick(Windows::UI::Xaml::Controls::ContentDialog^ Sender, Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs^ Event);
+
+        Sheet^ _Sheet;
+    };
+}
---
+++ Source/GUI/UWP/SheetEditFieldControl.xaml.h
@@ -0,0 +1,33 @@
+﻿//
+// SheetViewEditFieldControl.xaml.h
+// Declaration of the SheetViewEditFieldControl class
+//
+
+#pragma once
+
+#include "SheetEditFieldControl.g.h"
+
+namespace MediaInfo
+{
+    [Windows::Foundation::Metadata::WebHostHidden]
+    public ref class SheetEditFieldControl sealed
+    {
+    public:
+        SheetEditFieldControl(Platform::String^ Name, size_t Width, size_t StreamKind, size_t StreamPos, Platform::String^ Parameter);
+        Platform::String^ Get_Name();
+        size_t Get_Width();
+        size_t Get_StreamKind();
+        size_t Get_StreamPos();
+        Platform::String^ Get_Parameter();
+
+    private:
+        void Get_Streams();
+        void Get_Parameters();
+
+        static Platform::Collections::Map<size_t, Platform::String^>^ _Streams;
+        static Platform::Collections::Map<size_t, Windows::Foundation::Collections::IMapView<Platform::String^, Platform::String^>^>^ _Parameters;
+        void StreamBox_SelectionChanged(Platform::Object^ Sender, Windows::UI::Xaml::Controls::SelectionChangedEventArgs^ Event);
+        void ParameterBox_SelectionChanged(Platform::Object^ Sender, Windows::UI::Xaml::Controls::SelectionChangedEventArgs^ Event);
+        void NumText_TextChanging(Windows::UI::Xaml::Controls::TextBox^ Sender, Windows::UI::Xaml::Controls::TextBoxTextChangingEventArgs^ Event);
+    };
+}
